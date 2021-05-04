66f88f6c2707cc7f8de9ab0757aa0556648ae8bc
sonalcodexive <sonal.codexive@gmail.com>
2021-05-04 15:20:07 +0530

    css issue fix

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,8 +7,8 @@ const Header = (props) => {
     <Fragment>
         <header className={classes.header}>
             <h1>ReacteMaels</h1>
-            </header>
             <HeaderCartButton  onClick={props.onShowCart} />
+            </header>
             <div className={classes['main-image']}>
                 <img src={mealaImage} alt="A table full of delicious food" />
             </div>  
